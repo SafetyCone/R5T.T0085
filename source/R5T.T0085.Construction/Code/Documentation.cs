c1ec66f6b2b52d165d939c8a1fc96f8e88501b58
davidcoats <david@getsafetycone.com>
2021-11-09 11:02:39 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0085.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0085.Construction
+{
+    /// <summary>
+    /// Construction project for ApplicationSettingsJsonFile and other common, public IConfigurationSource types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0085/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0085
+{
+    /// <summary>
+    /// ApplicationSettingsJsonFile and other common, public IConfigurationSource types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
